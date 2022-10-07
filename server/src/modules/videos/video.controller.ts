13e13eb67afa054d9e2b43347de6bf2ee4c18df7
impolska742 <vaibhav19bhardwaj@gmail.com>
2022-10-07 23:20:32 +0530

    Streaming video in chunks

--- server/src/modules/videos/video.controller.ts
+++ server/src/modules/videos/video.controller.ts
@@ -7,6 +7,7 @@ import fs from 'fs'
 import { UpdateVideoBody, UpdateVideoParams } from './video.schema'
 
 const MIME_TYPES = ['video/mp4']
+const CHUNK_SIZE_IN_BYTES = 1000000 // 1mb
 
 function getPath({
     videoId,
@@ -96,4 +97,54 @@ export async function updateVideoHandler(
 export async function getPublishedVideos(_: Request, res: Response) {
     const videos = await findVideos()
     return res.status(StatusCodes.OK).send(videos)
+}
+
+export async function streamVideoHandler(req: Request, res: Response) {
+    const { videoId } = req.params
+    const range = req.headers.range
+
+    if (!range) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .send('Range must be provided')
+    }
+
+    const video = await findVideo(videoId)
+
+    if (!video) {
+        return res.status(StatusCodes.NOT_FOUND).send('Video not found')
+    }
+
+    const filePath = getPath({
+        videoId: video.videoId,
+        extension: video.extension,
+    })
+
+    const fileSizeInBytes = fs.statSync(filePath).size
+
+    const chunkStart = Number(range.replace(/\D/g, ''))
+
+    const chunkEnd = Math.min(
+        chunkStart + CHUNK_SIZE_IN_BYTES,
+        fileSizeInBytes - 1,
+    )
+
+    const contentLength = chunkEnd - chunkStart + 1
+
+    const headers = {
+        'Content-Range': `bytes ${chunkStart}-${chunkEnd}/${fileSizeInBytes}`,
+        'Accept-Ranges': `bytes`,
+        'Content-Length': contentLength,
+        'Content-Type': `video/${video.extension}`,
+        // 'Cross-Origin_Resource-Policy': 'cross-origin',
+    }
+
+    res.writeHead(StatusCodes.PARTIAL_CONTENT, headers)
+
+    const videoStream = fs.createReadStream(filePath, {
+        start: chunkStart,
+        end: chunkEnd,
+    })
+
+    videoStream.pipe(res)
 }
--- server/src/modules/videos/video.route.ts
+++ server/src/modules/videos/video.route.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import requireUser from '../../middlewares/requireUser'
 import {
     getPublishedVideos,
+    streamVideoHandler,
     updateVideoHandler,
     uploadVideoHandler,
 } from './video.controller'
@@ -11,5 +12,6 @@ const router = express.Router()
 router.post('/', requireUser, uploadVideoHandler)
 router.patch('/:videoId', requireUser, updateVideoHandler)
 router.get('/', getPublishedVideos)
+router.get('/:videoId', streamVideoHandler)
 
 export default router
