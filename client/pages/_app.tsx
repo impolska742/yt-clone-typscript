8b8a6bb9479e6e69c53949f7311e51865c2b1d44
impolska742 <vaibhav19bhardwaj@gmail.com>
2022-10-08 16:51:24 +0530

    Added mantine theme

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import Head from 'next/head'
+import { NotificationsProvider } from '@mantine/notifications'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>YouTube Clone</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <MantineProvider
+                withGlobalStyles
+                withCSSVariables
+                withNormalizeCSS
+                theme={{
+                    colorScheme: 'dark',
+                }}
+            >
+                <NotificationsProvider>
+                    <Component {...pageProps} />
+                </NotificationsProvider>
+            </MantineProvider>
+        </>
+    )
 }
 
 export default MyApp
